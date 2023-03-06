b7aa384973cb2def4ab7840e1c0cba21214f5c7c
alissonbatalha <alissonlsbatalhaa@gmail.com>
2023-03-06 17:18:30 +0000

    função de mostrar bandeira do cartão adicionada

--- js/script.js
+++ js/script.js
@@ -46,6 +46,70 @@ function showCardExpire () {
 function showCardCvc () {
   cardBackCvc.innerHTML = `${cardCvcInput.value}`;
 }
+
+function showCardFlag () {
+	let cardFlag = document.querySelector(".card-front__flag");
+	
+	let part1 = cardNumberInput.value.substring(0,4);
+	
+	//visa
+	if (part1[0] === '4') {
+		cardFlag.style.display = "block";
+		cardFlag.src = "./images/Visa-logo.png";
+	} 
+	
+	
+	//mastercard
+	else if (part1[0] === '5' && part1[1] === '1' || part1[0] === '5' && part1[1] === '2' || part1[0] === '5' && part1[1] === '3' || part1[0] === '5' && part1[1] === '4' || part1[0] === '5' && part1[1] === '5') {
+		
+		cardFlag.style.display = "block";
+		cardFlag.style.width = "30%";
+		cardFlag.style.top = "5%";
+		cardFlag.style.right = "1%";
+		cardFlag.src = "./images/mastercard-logo.png";
+	} 
+	
+	//dinersclub
+	else if (part1[0] === '3' && part1[1] === '6' || part1[0] === '3' && part1[1] === '8') {
+		
+		cardFlag.style.display = "block";
+		cardFlag.style.top = "3%";
+		cardFlag.src = "./images/diners-club-logo.png";
+	} 
+	
+	//discover
+	else if (part1[0] === '6' && part1[1] === '0' && part1[2] === '1' && part1[3] === '1' || part1[0] === '6' && part1[1] === '5') {
+
+		cardFlag.style.display = "block";
+		cardFlag.src = "./images/discover-logo.png";
+	} 
+	
+	//JCB
+	else if (part1[0] === '3' && part1[1] === '5') {
+
+		cardFlag.style.display = "block";
+		cardFlag.style.width = "19%";
+		cardFlag.src = "./images/jcb-logo.png";
+	} 
+	
+	//American Express
+	else if (part1[0] === '3' && part1[1] === '4' 
+	|| part1[0] === '3' && part1[1] === '7') {
+
+		cardFlag.style.display = "block";
+		cardFlag.style.width = "28%";
+		cardFlag.style.top = "3%";
+		cardFlag.style.right = "3%";
+		cardFlag.src = "./images/american-express-logo.png";
+	} 
+	
+}
+
+function hideFlag () {
+	let cardFlag = document.querySelector(".card-front__flag");
+	
+	cardFlag.style.display = "none";
+}
   
 
 
